67263ae05ec8636e6209552068040f36e4cf04bf
Sarah Squyres <ssquyres15@gmail.com>
2023-07-23 17:19:18 -0500

    create product, tag, category now functional

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -38,21 +38,14 @@ router.get('/:id', async (req, res) => {
 });
 
 // create new product
-router.post('/', (req, res) => {
-  /* req.body should look like this...
-    {
-      product_name: "Basketball",
-      price: 200.00,
-      stock: 3,
-      tagIds: [1, 2, 3, 4]
-    }
-  */
+router.post('/',  (req, res) => {
   Product.create(req.body)
     .then((product) => {
       // if there's product tags, we need to create pairings to bulk create in the ProductTag model
       if (req.body.tagIds.length) {
         const productTagIdArr = req.body.tagIds.map((tag_id) => {
           return {
+            
             product_id: product.id,
             tag_id,
           };
@@ -65,7 +58,7 @@ router.post('/', (req, res) => {
     .then((productTagIds) => res.status(200).json(productTagIds))
     .catch((err) => {
       console.log(err);
-      res.status(400).json(err);
+      res.status(400).json({ message: "Unable to create product", error: err });
     });
 });
 
